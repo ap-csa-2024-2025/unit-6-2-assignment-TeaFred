4567e12d189602a64ce5e5d3ec2dde05ec76ea6b
TeaFred <fhtruong100@student.hbuhsd.edu>
2025-01-17 11:19:58 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -18,9 +18,41 @@ public class Main
     System.out.println("Contains negative: " + containsNeg(arr));
   }
 
-  public static boolean containsNeg(int[] array)
+  public static boolean containsNeg(int[] arr)
   {
-    // Implement Problem 1 here
-    return 0;
+    for (int i = 0; i < arr.length; i++)
+    {
+      if (arr[i] <= 0)
+      {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public static int numDivisibleBy3 (int[] arr)
+  {
+    int threecounter;
+    for (int i = 0; i < arr.length; i++)
+    {
+      if (arr[i]%3 == 0)
+      {
+        threecounter++;
+      }
+    }
+      return threecounter;
+  }
+
+  public static int numDivisible (int[] arr, num)
+  {
+    int numcounter;
+    for (int i = 0; i < arr.length; i++)
+    {
+      if (arr[i]%numcounter == 0)
+      {
+        numcounter++;
+      }
+    }
+    return numcounter++;
   }
 }
